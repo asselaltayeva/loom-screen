c6fad25c78071838b1f3360a5d689742f54e8ee0
Assel <asselaltayeva@users.noreply.github.com>
2025-09-08 20:55:03 +0900

    auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,13 +1,86 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl font-karla'>
-      Auth
-      </h1>
-    </div>
-  )
-}
+    <main className="sign-in">
+      <aside className="testimonial">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Loom Logo"
+            width={32}
+            height={32}
+          />
+          <h1 className="text-2xl font-karla font-bold">Looma</h1>
+        </Link>
 
-export default Page
+        <div className="description">
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image
+                  src="/assets/icons/star.svg"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+            </figure>
+            <p>
+              Looma is great. I have been using to record videos and share with
+              my team. Great resource for video making and sharing.{" "}
+            </p>
+            <article>
+              <Image
+                src="/assets/images/jason.png"
+                alt="User"
+                width={64}
+                height={64}
+                className="rounded-full aspect-square"
+              />
+              <div>
+                <h2>Jason Hwang</h2>
+                <p>Product Manager, ImaginaryLabs</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p>© Looma {new Date().getFullYear()}</p>
+      </aside>
+
+      <aside className="google-sign-in">
+        <section>
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="Loom Logo"
+              width={40}
+              height={40}
+            />
+            <h1>Looma</h1>
+          </Link>
+
+          <p>
+            {" "}
+            Create and share your very first <span>Looma</span> video in
+            seconds!~
+          </p>
+          <button>
+            <Image
+              src="/assets/icons/google.svg"
+              alt="Google Logo"
+              width={22}
+              height={22}
+            />
+            Sign in with Google
+          </button>
+        </section>
+      </aside>
+      <div className="overlay"></div>
+    </main>
+  );
+};
+
+export default Page;
---
+++ app/(root)/video/[videoId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='wrapper page'>
+        VIDEO Details Page
+    </main>
+  )
+}
+
+export default page
