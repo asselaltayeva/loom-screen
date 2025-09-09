5b79eb07bc9aefcf2f8940024867996f6bd10505
Assel <asselaltayeva@users.noreply.github.com>
2025-09-09 15:30:26 +0900

    video, thumbnail upload

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,67 +1,89 @@
-"use client"
+"use client";
 
-import FileInput from '@/components/FileInput'
-import FormField from '@/components/FormField'
-import { useState, ChangeEvent, FormEvent, useEffect  } from 'react';
+import FileInput from "@/components/FileInput";
+import FormField from "@/components/FormField";
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
 const Page = () => {
-    const [formData, setFormData] = useState({
-        title: '',
-        description: '',
-        visibility:'public',
-    });
-    const [error, setError] = useState<string | null>(null);
-    const handleInputChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-      ) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({ ...prev, [name]: value }));
-      };
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    visibility: "public",
+  });
+
+  const video = {};
+  const thumbnail = {};
+
+  const [error, setError] = useState<string | null>(null);
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
-    <main className='wrapper-md upload-page'>
-        <h1>Upload a video</h1>
+    <main className="wrapper-md upload-page">
+      <h1>Upload a video</h1>
 
-        {error && (
-            <div className='error-field'>{error}</div>
-        )}
+      {error && <div className="error-field">{error}</div>}
 
-        <form className='rounded-20 shadow-20 gap-6 w-full flex flex-col px-5 py-7.5'>
-        <FormField 
-            id="title"
-            label="Title"
-            placeholder='Enter a clear title'
-            value={formData.title}
-            onChange={handleInputChange}
-        />
-        
-        <FormField 
-            id="description"
-            label="Description"
-            placeholder="Describe what this video is about"
-            value={formData.description}
-            as="textarea"
-            onChange={handleInputChange}
+      <form className="rounded-20 shadow-20 gap-6 w-full flex flex-col px-5 py-7.5">
+        <FormField
+          id="title"
+          label="Title"
+          placeholder="Enter a clear title"
+          value={formData.title}
+          onChange={handleInputChange}
         />
 
-        <FormField 
-            id="visibility"
-            label="Visibility"
-            value={formData.visibility}
-            as="select"
-            options={[
-                { label: "Public", value: "public" },
-                { label: "Private", value: "private" },
-            ]}
-            onChange={handleInputChange}
+        <FormField
+          id="description"
+          label="Description"
+          placeholder="Describe what this video is about"
+          value={formData.description}
+          as="textarea"
+          onChange={handleInputChange}
         />
 
+        <FileInput 
+            id="video"
+            label="Video File"
+            accept="video/*"
+            file={video.file}
+            previewUrl={video.previewUrl}
+            inputRef={video.inputRef}
+            onChange={video.handleFileChange}
+            onReset={video.resetFile}
+            type="video"
+        />
 
-        
-        <FileInput />
-        </form>
+        <FileInput 
+            id="thumbnail"
+            label="Thumbnail File"
+            accept="image/*"
+            file={thumbnail.file}
+            previewUrl={thumbnail.previewUrl}
+            inputRef={thumbnail.inputRef}
+            onChange={thumbnail.handleFileChange}
+            onReset={thumbnail.resetFile}
+            type="image"
+        />
+
+        <FormField
+          id="visibility"
+          label="Visibility"
+          value={formData.visibility}
+          as="select"
+          options={[
+            { label: "Public", value: "public" },
+            { label: "Private", value: "private" },
+          ]}
+          onChange={handleInputChange}
+        />
+      </form>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
